6c6a3745cc90405646fde9ba5912f64596827b82
graham-jay <jaygraham.mt@gmail.com>
2025-11-20 17:01:25 -0700

    simplifying mux and dff designs

--- src/rtl/dff.sv
+++ src/rtl/dff.sv
@@ -1,11 +1,8 @@
 module dff (
-    input  clk, rst,
+    input  clk,
     input  D,
-    output Q,
+    output reg Q
 );
     always @(posedge clk)
-        if (rst)
-            assign Q = 0;
-        else
-            assign Q = D;
+        Q <= D;
 endmodule
--- src/rtl/mux2.sv
+++ src/rtl/mux2.sv
@@ -2,9 +2,5 @@ module mux2 (
     input  in1, in2, sel,
     output out
 );
-    if (sel) begin
-        assign out = in2;
-    end else begin
-        assign out = in1;
-    end
+    assign out = sel ? in2 : in1;
 endmodule
