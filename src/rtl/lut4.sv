2d42456decdbecf6b4ec570b3b285e6be787b020
graham-jay <jaygraham.mt@gmail.com>
2025-12-11 21:59:56 -0700

    changing lut inputs to bus

--- src/rtl/lut4.sv
+++ src/rtl/lut4.sv
@@ -1,7 +1,7 @@
 module lut4 (
-    input  in1, in2, in3, in4,
+    input  [3:0]  in,
     input  [15:0] sram,
     output out
 );
-    assign out = sram[{in1,in2,in3,in4}];
+    assign out = sram[in];
 endmodule
